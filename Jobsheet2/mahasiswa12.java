638f4238909e34edcc913ed4ff401048f5a3a553
ocaocuoci <hernandar0606@gmail.com>
2025-02-19 08:24:19 +0700

    percobaan 3

--- Jobsheet2/mahasiswa12.java
+++ Jobsheet2/mahasiswa12.java
@@ -1,44 +1,59 @@
 import java.util.Scanner;
-public class mahasiswa12{
-    static String nim, nama, kelas;
-    static double ipk;
-    static Scanner sc =new Scanner(System.in);
 
-    public static void main(String[] args){
+public class mahasiswa12 {
+    // Atribut
+    String nim, nama, kelas;
+    double ipk;
 
-        tampilkanInformasi();
-        
+    // Konstruktor Default (Tanpa Parameter)
+    public mahasiswa12() {
+        this.nama = "";
+        this.nim = "";
+        this.kelas = "";
+        this.ipk = 0.0;
     }
-    public static void tampilkanInformasi(){
-        System.out.println("Nama: " + nama);
-        System.out.println("NIM: " + nim);
-        System.out.println("IPK: " + ipk);
-        System.out.println("Kelas: " + kelas);
 
+   
+    public mahasiswa12(String nama, String nim, String kelas, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.ipk = ipk;
     }
-    public static void ubahKelas(String kelasBaru){
-        kelas = kelasBaru;
+
+    
+    public void tampilkanInformasi() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
     }
-    public static void updateIPK(double ipkBaru){
-        if(ipkBaru >= 0.0 && ipkBaru <= 4.0){
-            ipk = ipkBaru;
+
+    
+    public void ubahKelas(String kelasBaru) {
+        this.kelas = kelasBaru;
+    }
+
+    
+    public void updateIPK(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            this.ipk = ipkBaru;
             System.out.println("IPK berhasil diperbarui");
-        } else{
+        } else {
             System.out.println("IPK tidak valid. Harus antara 0.0 sampai 4.0");
         }
-        
-    } 
-    public static String nilaiKinerja(){   
-    
-        if (ipk >= 3.5){
+    }
+
+     
+    public String nilaiKinerja() {
+        if (ipk >= 3.5) {
             return "Kinerja Sangat Baik";
-        } else if (ipk >= 3.0){
+        } else if (ipk >= 3.0) {
             return "Kinerja Baik";
-        } else if(ipk >= 2.0){
+        } else if (ipk >= 2.0) {
             return "Kinerja Cukup";
         } else {
             return "Kinerja Kurang";
         }
     }
-    
 }
--- Jobsheet2/mahasiswaMain12.java
+++ Jobsheet2/mahasiswaMain12.java
@@ -1,18 +1,31 @@
-public class mahasiswaMain12{
-    public static void main(String[] args){
-
+public class mahasiswaMain12 {
+    public static void main(String[] args) {
+       
         mahasiswa12 mhs1 = new mahasiswa12();
 
+        
         mhs1.nama = "Hernanda Rizka Utami";
         mhs1.nim = "244107060075";
         mhs1.kelas = "SIB 1B";
         mhs1.ipk = 3.58;
 
+      
         mhs1.tampilkanInformasi();
 
-
+    
         mhs1.ubahKelas("SIB 1J");
         mhs1.updateIPK(3.89);
+
+      
         mhs1.tampilkanInformasi();
+
+        System.out.println(); 
+
+        
+        mahasiswa12 mhs2 = new mahasiswa12("Akhrunisa Meylodita W.", "24410706087", "SIB 1D", 3.76);
+
+       
+        mhs2.updateIPK(3.50);
+        mhs2.tampilkanInformasi();
     }
 }
