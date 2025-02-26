70dcaf7bf751e652997b43189de349dd2e4d146f
ocaocuoci <hernandar0606@gmail.com>
2025-02-26 15:41:57 +0700

    percobaan 3 empat

---
+++ Praktikum03/Matakuliah.java
@@ -0,0 +1,13 @@
+public class MataKuliah{
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahSks;
+
+    public MataKuliah(String kode, String nama, int sks, int jumlahSks){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahSks = jumlahSks;
+    }
+}
