3e7b23c02c9e2f427a57126bb18ba5b299620c9d
ocaocuoci <hernandar0606@gmail.com>
2025-02-28 19:26:35 +0700

    percobaan 3 pertanyaan 1, 2, 3

--- Praktikum03/MataKuliahDemo.java
+++ Praktikum03/MataKuliahDemo.java
@@ -4,6 +4,7 @@ public class MataKuliahDemo{
 
         Scanner sc = new Scanner(System.in);
         MataKuliah[] arrayOfMataKuliah = new MataKuliah[3];
+
         String kode, nama, dummy;
         int sks, jumlahJam;
 
@@ -21,17 +22,26 @@ public class MataKuliahDemo{
             jumlahJam = Integer.parseInt(dummy);
             System.out.println("-----------------------------");
 
-            arrayOfMataKuliah[i] = new MataKuliah(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah[i] = new MataKuliah(" ", " ", 0, 0);
+            arrayOfMataKuliah[i].tambahData(kode, nama, sks, jumlahJam);
         }
+        
 
-        for(int i = 0; i<3; i++){
+       for(int i = 0; i<3; i++){
             System.out.println("Data Mata Kuliah ke-" + (i+1));
             System.out.println("Kode        :" + arrayOfMataKuliah[i].kode);
             System.out.println("Nama        :" + arrayOfMataKuliah[i].nama);
             System.out.println("SKS         :" + arrayOfMataKuliah[i].sks);
             System.out.println("Jumlah Jam  :" + arrayOfMataKuliah[i].jumlahJam);
+
             System.out.println("-----------------------------------------------");
+        } 
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("Data Mata Kuliah ke-" + (i+1));
+            arrayOfMataKuliah[i].cetakInfo();
         }
+        sc.close();
 
     }
 }
--- Praktikum03/Matakuliah.java
+++ Praktikum03/Matakuliah.java
@@ -4,7 +4,7 @@ class MataKuliah {
     int sks;
     int jumlahJam;
 
-
+    //konstruktor semua berparameter
     public MataKuliah(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
@@ -12,6 +12,45 @@ class MataKuliah {
         this.jumlahJam = jumlahJam;
     }
 
+    //konstruktor tanpa parameter
+    public MataKuliah(){
+        this.kode = "Belum ada";
+        this.nama = "Tidak diketahui";
+        this.sks = 0;
+        this.jumlahJam = 0;
+    }
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = sks * 2;
+    }
+
+    //konstruktor hanya kode dan nama
+    public MataKuliah(String kode, String nama){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = 0;
+        this.jumlahJam = 0;
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam; 
+    }
+
+    public void cetakInfo(){
+        System.out.println("Kode        : " + kode);
+        System.out.println("Nama        : " + nama);
+        System.out.println("SKS         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+        System.out.println("----------------------------");
+    }
+
 }
+
 
 
