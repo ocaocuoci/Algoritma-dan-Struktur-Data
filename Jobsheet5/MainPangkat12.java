0d6194bed374d9c174c19101f4b38e16e0d3faf8
ocaocuoci <hernandar0606@gmail.com>
2025-03-12 12:56:56 +0700

    percobaan 2

---
+++ Jobsheet5/MainPangkat12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class MainPangkat12{
+    public static void main(String [] args){
+    Scanner input12 = new Scanner(System.in);
+
+    System.out.print("Masukkan jumlah elemen: ");
+    int elemen = input12.nextInt();
+
+    Pangkat12[] png =new Pangkat12[elemen];
+    for(int i=0; i < elemen; i++){
+        System.out.print("Masukkan nilai basis elemen ke-" + (i+1)+ ": ");
+        int basis = input12.nextInt();
+        System.out.print("Masukkan nilai pangkat elemen ke-" + (i+1)+ ": ");
+        int pangkat = input12.nextInt();
+        png[i] = new Pangkat12(basis, pangkat);
+    }
+    System.out.println("HASIL PANGKAT BRUTE FORCE: ");
+    for(Pangkat12 p : png){
+       System.out.println(p.n + "^" + p.p + ": " + p.pangkatBF(p.n, p.p));
+    }
+    System.out.println("HASIL PANGKAT DIVIDE CONQUER: ");
+    for(Pangkat12 p : png){
+       System.out.println(p.n + "^" + p.p + ": " + p.pangkatDC(p.n, p.p));
+    } 
+}
+}
---
+++ Jobsheet5/Pangkat12.java
@@ -0,0 +1,28 @@
+public class Pangkat12{
+
+    int p, n;
+    Pangkat12(int n, int p){
+        this.n = n;
+        this.p = p;
+    }
+    public int pangkatBF(int a, int n){
+        int hasil = 1;
+        for(int i=0; i<n; i++){
+            hasil = hasil*a;
+        }
+        return hasil;
+    }
+    public int pangkatDC(int a, int n){
+        if(n==0){
+            return 1;
+        } else if(n==1){
+            return a;
+        } else {
+            if(n%2==1){
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2)*a);
+            } else{
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2));
+            }
+        }
+    }
+}
