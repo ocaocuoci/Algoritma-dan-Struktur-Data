f7bf6aa18617d080036874d6c610e8f16085300c
ocaocuoci <hernandar0606@gmail.com>
2025-03-25 22:02:31 +0700

    js6 pertemuan7 latihan 1

--- Praktikum05/mahasiswaDemo12.java
+++ Praktikum05/mahasiswaDemo12.java
@@ -3,13 +3,11 @@ public class mahasiswaDemo12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = sc.nextInt();
-        sc.nextLine(); 
+        int jumMhs = 5;
 
-        mahasiswaBerprestasi12 list = new mahasiswaBerprestasi12(jumlah);
+        mahasiswaBerprestasi12 list = new mahasiswaBerprestasi12(jumMhs);
 
-        for (int i = 0; i < jumlah; i++) {
+        for (int i = 0; i < jumMhs; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i + 1) + ":");
             System.out.print("Nama   : ");
             String nama = sc.nextLine();
@@ -23,6 +21,20 @@ public class mahasiswaDemo12 {
             
             list.tambah(new mahasiswa12(nim, nama, kelas, ipk));
         }
+        list.tampil();
+        System.out.println("----------------------------------");
+        System.out.println("Pencaarian Data");
+        System.out.println("----------------------------------");
+        System.out.println("masukkan ipk mahaiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
+
+        System.out.println("menggunakan sequential searching ");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
+        
 
         System.out.println("\nData mahasiswa sebelum sorting: ");
         list.tampil();
