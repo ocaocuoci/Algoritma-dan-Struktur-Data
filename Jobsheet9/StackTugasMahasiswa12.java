972ceef1209b28b3ce75c0fbcb25b28d9888f812
ocaocuoci <hernandar0606@gmail.com>
2025-04-30 14:22:56 +0700

    P1 pertanyaan 1

--- Jobsheet9/StackTugasMahasiswa12.java
+++ Jobsheet9/StackTugasMahasiswa12.java
@@ -54,7 +54,7 @@ public class StackTugasMahasiswa12{
     }
 
     public void print(){
-        for(int i = 0; i <= top; i++){
+        for(int i = top; i >= 0; i--){
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.print(" ");
