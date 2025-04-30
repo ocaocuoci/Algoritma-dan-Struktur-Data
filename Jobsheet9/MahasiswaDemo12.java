e6843725b88b5f72cf1740e0a403e03b7d7acca1
ocaocuoci <hernandar0606@gmail.com>
2025-04-30 14:39:28 +0700

    P1 pertanyaan 4

--- Jobsheet9/MahasiswaDemo12.java
+++ Jobsheet9/MahasiswaDemo12.java
@@ -13,6 +13,7 @@ public class MahasiswaDemo12{
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Tugas Terbawah");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
@@ -51,6 +52,13 @@ public class MahasiswaDemo12{
                 System.out.println("Daftar semua tugas: ");
                 System.out.println("Nama\tNim\tkelas");
                 stack.print();
+                break;
+
+                case 5:
+                Mahasiswa12 bawah = stack.bottom();
+                if(bawah != null){
+                    System.out.println("Tugas pertama dikumpulkan oleh " + bawah.nama);
+                }
                 break;
 
                 default:
--- Jobsheet9/StackTugasMahasiswa12.java
+++ Jobsheet9/StackTugasMahasiswa12.java
@@ -53,6 +53,15 @@ public class StackTugasMahasiswa12{
         }
     }
 
+    public Mahasiswa12 bottom(){
+        if(!isEmpty()){
+            return stack[0];
+        }else{
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
     public void print(){
         for(int i = top; i >= 0; i--){
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
