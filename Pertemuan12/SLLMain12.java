3cfa7729361ee08c58fffe2296be78acbe74d7b1
ocaocuoci <hernandar0606@gmail.com>
2025-05-19 16:12:01 +0700

    percobaan 2 modifikasi

--- Pertemuan12/SLLMain12.java
+++ Pertemuan12/SLLMain12.java
@@ -1,48 +1,30 @@
 import java.util.Scanner;
-public class SLLMain12{
-    public static void main (String[]args){
+
+public class SLLMain12 {
+    public static void main(String[] args) {
         SingleLinkedList12 sll = new SingleLinkedList12();
         Scanner sc = new Scanner(System.in);
 
-        sll.print();
+        Mahasiswa12 mhs4 = new Mahasiswa12("244107060075", "Dirga", "4D", 3.6);
+        Mahasiswa12 mhs3 = new Mahasiswa12("244107060056", "Alvaro", "1A", 4.0);
+        Mahasiswa12 mhs1 = new Mahasiswa12("244107060023", "Cintia", "3C", 3.5);
+        Mahasiswa12 mhs2 = new Mahasiswa12("244107060123", "Bimon", "2B", 3.8);
 
-        System.out.print("Masukkan jumlah mahasiswa:");
-        int jumlahMahasiswa = sc.nextInt();
-        sc.nextLine(); 
-
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            System.out.println("\nMasukkan data mahasiswa ke-" + (i + 1) + ":");
-            System.out.print("Nim: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = sc.nextDouble();
-            sc.nextLine(); 
-
-            Mahasiswa12 mahasiswa = new Mahasiswa12(nim, nama, kelas, ipk);
-            sll.addLast(mahasiswa);
-            sll.print();
-        }
-
-        Mahasiswa12 mhs1 = new Mahasiswa12("244107060075", "Dirga", "4D", 3.6);
-        Mahasiswa12 mhs2 = new Mahasiswa12("244107060056", "Alvaro", "1A", 4.0);
-        Mahasiswa12 mhs3 = new Mahasiswa12("244107060023", "Cintia", "3C", 3.5);
-        Mahasiswa12 mhs4 = new Mahasiswa12("244107060123", "Bimon", "2B", 3.8);
-
-        sll.print();
-        sll.addFirst(mhs1);
-
-        sll.print();
+        sll.addFirst(mhs4);
+        sll.addLast(mhs1);
         sll.addLast(mhs2);
-        
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
+        sll.addLast(mhs3);
 
-        sll.insertAt(3, mhs4);
-        sll.print();
+        System.out.println("Data indeks 1: ");
+        sll.getData(1);
 
+        System.out.println("Data mahasiswa an Bimon berada pada indeks: " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
