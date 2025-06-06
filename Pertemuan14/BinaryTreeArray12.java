32650d56be694d97e7faadbf1ff74bf1896ca212
ocaocuoci <hernandar0606@gmail.com>
2025-06-06 10:22:24 +0700

    tugas 4 binary trre array

--- Pertemuan14/BinaryTreeArray12.java
+++ Pertemuan14/BinaryTreeArray12.java
@@ -3,21 +3,41 @@ public class BinaryTreeArray12 {
     int idxLast;
 
     public BinaryTreeArray12() {
-        this.dataMahasiswa = new Mahasiswa12[10];
+        this.dataMahasiswa = new Mahasiswa12[10]; 
+        this.idxLast = -1; 
     }
 
-    void populateData(Mahasiswa12 dataMhs[], int idxLast) {
+    
+    void populateData(Mahasiswa12[] dataMhs, int idxLast) {
         this.dataMahasiswa = dataMhs;
         this.idxLast = idxLast;
     }
 
+    
+    public void add(Mahasiswa12 data) {
+        if (idxLast < dataMahasiswa.length - 1) {
+            idxLast++;
+            dataMahasiswa[idxLast] = data;
+        } else {
+            System.out.println("Tree penuh, tidak bisa menambahkan data.");
+        }
+    }
+
+    // Traversal InOrder (Left, Root, Right)
     void traverseInOrder(int idxStart) {
-        if (idxStart <= idxLast) {
-            if (dataMahasiswa[idxStart] != null) {
-                traverseInOrder(2 * idxStart + 1);
-                dataMahasiswa[idxStart].tampilInformasi();
-                traverseInOrder(2 * idxStart + 2);
-            }
+        if (idxStart <= idxLast && dataMahasiswa[idxStart] != null) {
+            traverseInOrder(2 * idxStart + 1);
+            dataMahasiswa[idxStart].tampilInformasi();
+            traverseInOrder(2 * idxStart + 2);
+        }
+    }
+
+    // Traversal PreOrder (Root, Left, Right)
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast && dataMahasiswa[idxStart] != null) {
+            dataMahasiswa[idxStart].tampilInformasi();
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
         }
     }
 }
--- Pertemuan14/BinaryTreeArrayMain12.java
+++ Pertemuan14/BinaryTreeArrayMain12.java
@@ -2,20 +2,20 @@ public class BinaryTreeArrayMain12 {
     public static void main(String[] args) {
         BinaryTreeArray12 bta = new BinaryTreeArray12();
 
-        Mahasiswa12 mhs1 = new Mahasiswa12("244160121", "Ali", "A", 3.57);
-        Mahasiswa12 mhs2 = new Mahasiswa12("244160185", "Candra", "C", 3.41);
-        Mahasiswa12 mhs3 = new Mahasiswa12("244160221", "Badar", "B", 3.75);
-        Mahasiswa12 mhs4 = new Mahasiswa12("244160220", "Dewi", "B", 3.35);
+        // Tambah data mahasiswa satu per satu menggunakan method add()
+        bta.add(new Mahasiswa12("244160121", "Ali", "A", 3.57));
+        bta.add(new Mahasiswa12("244160185", "Candra", "C", 3.41));
+        bta.add(new Mahasiswa12("244160221", "Badar", "B", 3.75));
+        bta.add(new Mahasiswa12("244160220", "Dewi", "B", 3.35));
+        bta.add(new Mahasiswa12("244160131", "Devi", "A", 3.48));
+        bta.add(new Mahasiswa12("244160205", "Ehsan", "D", 3.61));
+        bta.add(new Mahasiswa12("244160170", "Fizi", "B", 3.86));
 
-        Mahasiswa12 mhs5 = new Mahasiswa12("244160131", "Devi", "A", 3.48);
-        Mahasiswa12 mhs6 = new Mahasiswa12("244160205", "Ehsan", "D", 3.61);
-        Mahasiswa12 mhs7 = new Mahasiswa12("244160170", "Fizi", "B", 3.86);
-
-        Mahasiswa12[] dataMahasiswas = {mhs1, mhs2, mhs3, mhs4, mhs5, mhs6, mhs7, null, null};
-        int idxLast = 6;
-
-        bta.populateData(dataMahasiswas, idxLast);
-        System.out.println("\nInorder Traversal Mahasiswa: ");
+        // Tampilkan data mahasiswa menggunakan traversal
+        System.out.println("\nInorder Traversal Mahasiswa:");
         bta.traverseInOrder(0);
+
+        System.out.println("\nPreorder Traversal Mahasiswa:");
+        bta.traversePreOrder(0);
     }
 }
