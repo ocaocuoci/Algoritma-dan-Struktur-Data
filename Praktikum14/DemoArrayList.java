9609912eec0de07443a146f80774e14ba9d436fa
ocaocuoci <hernandar0606@gmail.com>
2025-06-11 15:06:37 +0700

    no 5

---
+++ Praktikum14/DemoArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class DemoArrayList{
+    public static void main(String[] args){
+
+        ArrayList<Customer> customer = new ArrayList<>(2);
+        Customer customer1 = new Customer(1, "Zaskia");
+        Customer customer2 = new Customer(5, "Budi");
+
+        customer.add(customer1);
+        customer.add(customer2);
+
+        customer.add(new Customer(4, "Cica"));
+
+        for(Customer cust : customer){
+            System.out.println(cust.toString());
+        }
+    }
+}
